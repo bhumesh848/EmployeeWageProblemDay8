63328b9bf29de985c5ae732f78cdfc2351314103
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2024-11-30 11:35:44 +0530

    Uc7 Created methods for emp wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,50 +3,66 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class EmployeeWage {
+
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
-    public static final int PART_TIME_HOUR = 8;
+    public static final int PART_TIME_HOUR = 4;
     public static final int WORKING_DAYS_PER_MONTH = 20;
 
+    public static int calculateDailyWage() {
+        return WAGE_PER_HOUR * FULL_DAY_HOUR;
+    }
+
+    public static int calculatePartTimeWage(int partTimeWagePerHour) {
+        return PART_TIME_HOUR * partTimeWagePerHour;
+    }
+
+    public static int calculateMonthlyWage() {
+        return WORKING_DAYS_PER_MONTH * calculateDailyWage();
+    }
+
+    public static boolean isEmployeePresent() {
+        return Math.random() < 0.5;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation program");
-        double randomNumber = Math.random() * 2;
-        System.out.println("random number generated "+ randomNumber);
-        int result = (int) randomNumber;
-        System.out.println("result : "+ result);
-        if (result == 1){
+        System.out.println("Welcome to the Employee Wage Computation Program");
+
+        if (isEmployeePresent()) {
             System.out.println("Employee is present");
-        }
-        else {
+        } else {
             System.out.println("Employee is absent");
         }
 
-        int dailyEmpWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        System.out.println("Daily Employee Wage is "+ dailyEmpWage);
+        int dailyWage = calculateDailyWage();
+        int partTimeWage = calculatePartTimeWage(15);
+        int monthlyWage = calculateMonthlyWage();
 
-        int partTimeWagePerhour = 15;
-        int partTimeWage = PART_TIME_HOUR * partTimeWagePerhour;
-        System.out.println("Part time employe wage is " + partTimeWage);
+        System.out.println("Daily Employee Wage: " + dailyWage);
+        System.out.println("Part-Time Employee Wage: " + partTimeWage);
+        System.out.println("Monthly Employee Wage: " + monthlyWage);
 
-        System.out.println("Press 1 to see daily employee wage");
-        System.out.println("Press 2 to see part time employee wage");
+        System.out.println("\nPress 1 to see Daily Employee Wage");
+        System.out.println("Press 2 to see Part-Time Employee Wage");
+        System.out.println("Press 3 to see Monthly Employee Wage");
 
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+        int choice = sc.nextInt();
 
-        switch (number){
-            case 1 :
-                System.out.println("Daily Employee Wage is "+ dailyEmpWage);
+        switch (choice) {
+            case 1:
+                System.out.println("Daily Employee Wage: " + dailyWage);
                 break;
             case 2:
-                System.out.println("part Time Employe wage is "+ partTimeWage);
+                System.out.println("Part-Time Employee Wage: " + partTimeWage);
+                break;
+            case 3:
+                System.out.println("Monthly Employee Wage: " + monthlyWage);
                 break;
             default:
-                System.out.println("Invalid Input , press 1 or 2 .");
+                System.out.println("Invalid input. Please press 1, 2, or 3.");
         }
 
-        int wageForMonth = WORKING_DAYS_PER_MONTH * dailyEmpWage;
-        System.out.println("Wages for a month would be " + wageForMonth);
+        sc.close();
     }
 }
